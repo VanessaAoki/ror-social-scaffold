92d1ff3281f0ed5d40120e2c219b176e04f08102
emmy <emmy66418@gmail.com>
2021-05-11 14:46:39 +0200

    Create a friends method in the User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  # Friendship
+  has_many :friendships, -> { where(status: 'confirmed') }
+  has_many :friends, through: :friendships
+
+  def friend?(user)
+    friendship = Friendship.find_by(user_id: user.id, friend_id: id, status: 'confirmed') ||
+                 Friendship.find_by(user_id: id, friend_id: user.id, status: 'confirmed')
+    true unless friendship.nil?
+  end
 end
