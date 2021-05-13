c8560dc383a6e6bc729747d454c9c61a4170e227
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-12 23:12:54 -0300

    Add friendship helper, controller and form

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,54 @@
+class FriendshipController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @friendships = Friendship.all
+  end
+
+  def new
+    @friendship = Friendship.new(friendship_params)  
+  end
+
+  def create
+    @friendship = Friendships.create!(friendship_params)
+
+    if @friendship.save
+      redirect_to users_path, notice: 'Friend request sent.'
+    else
+      render 'new'
+    end
+  end
+
+  def confirm
+    friendship = Friendship.find(params[:id])
+    friendship.status = true
+    friendship.save
+    redirect_to users_path
+  end
+
+  def reject
+    friendship = Friendship.find(params[:id])
+    friendship.status = false
+    user = User.find(friendship.user_id)
+    friend = User.find(friendship.friend_id)
+    Friendship.where(user: user, friend: friend).first.delete
+    friendship.save
+    redirect_to users_path
+  end
+
+  def destroy
+    friendship = friendship.find_by(id: params[:id], user: current_user, friend_id: params[:user_id])
+    if friendship
+      friendship.destroy
+      redirect_to posts_path, notice: "You are not friends with #{friend.name} anymore."
+    else
+      redirect_to posts_path, alert: 'You cannot disfriendship post that you did not friendship before.'
+    end
+  end
+
+private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id, :confirmed, :id)
+  end
+end
