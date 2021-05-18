1518318065da651dca965da50a41e599ae4e572f
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-18 17:13:12 -0300

    Add the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,30 +9,16 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
   has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :confirmed_friendships, -> { where status: true }, class_name: 'Friendship'
+  has_many :friends, through: confirmed_friendships
 
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.status }
-    friends_array.compact
-  end
+  has_many :pending_friendships, -> { where status: nil }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: pending_friendships, source: friend
 
-  # Users who have yet to confirme friend requests
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.status }.compact
-  end
-
-  # Users who have requested to be friends
-  def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
-  end
-
-  def confirm_friend(user)
-    friendship = inverse_friendships.find { |_friendship| friendship.user == user }
-    friendship.status = true
-    friendship.save
-  end
+  has_many :inverted_friendships, -> { where status: nil }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :friend_requests, through: inverted_friendships, source: friend
 
   def friend?(user)
     friends.include?(user)
