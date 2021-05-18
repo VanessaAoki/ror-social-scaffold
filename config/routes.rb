b1407000bd0e5a20239a7a23aa14c5ada77bcd6f
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-18 19:56:40 -0300

    Fix users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
+
   root 'posts#index'
 
   devise_for :users
 
   resources :users, only: [:index, :show] do
-    member do
-      post 'accept', to: 'friendships#confirm'
-      post 'reject', to: 'friendships#reject'
-    end
+    resources :friendships, only: [:new, :create, :update, :destroy]
   end
-
+  
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
   end
-  
-  resources :friendships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
