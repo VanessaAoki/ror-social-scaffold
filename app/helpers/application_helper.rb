54dcf074797f96adf6e6c679719adff67a27cb60
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-22 21:38:30 -0300

    Style new post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,17 +11,17 @@ module ApplicationHelper
     out = ''
     if user_signed_in?
       out += '<p>'
-      out += link_to current_user.name, edit_user_registration_path, class: 'button ml-2 is-pink'
+      out += link_to current_user.name, edit_user_registration_path, class: 'button ml-2 is-navbar'
       out += '</p>'
       out += '<p>'
-      out += link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button ml-2 is-pink'
+      out += link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button ml-2 is-navbar'
     else
       out += '</p>'
       out += '<p>'
-      out += link_to 'Log In', new_user_session_path, class: 'button is-pink'
+      out += link_to 'Log In', new_user_session_path, class: 'button is-navbar'
       out += '</p>'
       out += '<p>'
-      out += link_to 'Sign Up', new_user_registration_path, class: 'button is-pink'
+      out += link_to 'Sign Up', new_user_registration_path, class: 'button is-navbar'
       out += '</p>'
     end
     out.html_safe
